fe4b2c29b52772b0335635afb76e1b04288938c8
Aina <ainaard@mail.ru>
2019-12-06 22:43:57 +0300

    added docker-compose file

--- SpringBoot/src/main/java/ru/itis/controllers/LoginController.java
+++ SpringBoot/src/main/java/ru/itis/controllers/LoginController.java
@@ -3,8 +3,6 @@ package ru.itis.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
